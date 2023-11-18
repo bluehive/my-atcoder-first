0fb62562a4c6223e37a3ee8c471d118604418cf1
goalwill <goalwill@gmail.com>
2023-11-18 09:46:42 +0900

    use regex ...

--- main.cpp
+++ main.cpp
@@ -1,14 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define rep(i,n) for (int i = 0; i < (int)(n) ; i++)
 
-#define rep(i, n) for (int i = 0; i <(int)(n); i++)
-int main(){
-
-    int x = 5;
-    x += 1 + 2 ;
-    cout << x << endl;
-    rep (i , 5){
-        cout << x++ << "::" ;
+int main() {
+    int N, A;
+    cin >> N >> A;
+    assert( not(N < 0 || 7 < N) );  assert(not (A < 0 || 10 < A) ) ;
+    // 正規表現を定義  std::regex pattern("+|-|*|/");
+    std::regex pattern("[+\\-*/]");
+    std::string op; int b;
+    rep(i, N){
+        std::cin >> op >> b ;
+        // 正規表現にマッチするかチェック
+        assert(std::regex_match(op, pattern));
+        // ここにプログラムを追記
+        if (op == "/" && b == 0) {
+            cout << "error" << endl;
+            break ;
+        }
+        cout << i + 1 << ":" << (A = (op == "+") ? A + b : (op == "-") ? A - b : (op == "*") ? A * b : A / b) << endl ;
     }
 
+    return 0;
 }
