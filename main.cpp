652489ccac90e3fb98b6928e2991bde0c78a1734
goalwill <goalwill@gmail.com>
2023-11-26 07:24:10 +0900

    lambda use 再帰...

--- main.cpp
+++ main.cpp
@@ -13,24 +13,41 @@ int fibo(int i){
     }
 }
 
-int main(){
-    long int N ;
-    N = 12089000 ;// cin >> N;
-    std::vector<int> v1(9,0) ;
-    auto mydump = [](auto x){ cout << x << endl;};
-    assert ( 0 < N) ;  assert (N <= pow(10, 8) ); //　&& "Error assert !!" );
-    int  temp = N ;
-    rep(i, N * 10){
-        v1[i] = N % 10;
-        N /= 10;
-    }
+/* x = 1
+ * A func : x *= 2
+   B func : x += K
+   cin >> N , K
+   1 <= N K <= 10 , int
+   rep i,N {
+     if A > B   Bfunc
+     else if A < B  Afunc...
+     else ...
+   }
+   コレクションは　ベクトルか
+   再帰はないか
 
-    int num = 0;
-    for(const auto& i : v1){
-        num += i ;
-    }
-    //mydump(num) ;
-    temp % num == 0 ? mydump("Yes") : mydump("No");
-    mydump(fibo(22));
+*/
+
+int main(){
+    int N, K ;
+    int init = 1;
+    cin >> N >> K;
+    auto mydmp = [](auto x){ cout << x << endl;};
+    assert ( 0 < N && N <= pow(10, 1) );    assert ( 0 < K && K <= pow(10, 1) );
+    auto Afunc = [](auto x){     return x *= 2 ;    };
+    auto Bfunc = [](auto x,auto y){     return x +=  y ;    };
+    auto refunc = [](int i , auto A, auto B){
+        if (i == 0){
+            return 1 ;
+        }else{
+            if (A > B){
+                return refunc(i - 1 ,B ,A)
+            }else{
+                return refunc(i - 1 ,A,B)
+            }
+        }
+    };
+    //
+    mydump(refunc(N,Afunc(init),Bfunc(init,K)));
     return 0;
 }
